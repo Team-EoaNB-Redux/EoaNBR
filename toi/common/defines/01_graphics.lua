ac407cfc76754f59e92e88ef77e3c79c8bd68ed1
Vanadium <67757372+Vanadium-GITHUB@users.noreply.github.com>
2022-11-03 21:32:33 -0700

    1.12.5

---
+++ toi/common/defines/01_graphics.lua
@@ -0,0 +1,8 @@
+NDefines_Graphics.NGraphics.MINIMUM_PROVINCE_SIZE_IN_PIXELS = 1 -- 8
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 24000
+-- NDefines_Graphics.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 512
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 8196
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 2048
+
+-- -- NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_WIDTH = 10 -- depreciated
+-- -- NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_WIDTH = 41 -- depreciated
