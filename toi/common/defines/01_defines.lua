72b015a10696945eb51618820588b919e35d086f
Energywelder <67761668+Energywelder@users.noreply.github.com>
2026-05-30 21:40:09 -0400

    generic stuff
    -Garrisons now favor light infantry over light cav, wtf
    -Civs are more productive after the civ purge, hopefully this will negate much of the damage of the purge
    -arms trade is easier
    -certain unbalanced occupation law has been locked behind soc-egal ideology
    -moved buildings l english

--- toi/common/defines/01_defines.lua
+++ toi/common/defines/01_defines.lua
@@ -22,6 +22,7 @@ NDefines.NDiplomacy.BASE_IMPROVE_RELATION_DIFFERENT_IDEOLOGY_GROUP_MAINTAIN_COST
 NDefines.NDiplomacy.PEACE_SCORE_SCALE_FACTOR = 1.25 -- 1.35
 NDefines.NDiplomacy.MARKET_ACCESS_ACCEPTANCE_OPINION = 2.5 -- Acceptance factor for opinion
 NDefines.NDiplomacy.MARKET_ACCESS_ACCEPTANCE_TRADE_INFLUENCE = 0.00 -- Acceptance factor for trade influence (adjusted from base value)
+NDefines.NDiplomacy.EQUIPMENT_PURCHASE_ACCEPTANCE_TRADE_INFLUENCE = 0.00 -- Acceptance factor for trade influence (adjusted from base value)
 
 NDefines.NCountry.BASE_RESEARCH_SLOTS = 1
 NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.010
